b142e7036fd3f3596180f656362d5ecb6621fac8
aspeziale <aspeziale@digitalidentity.tech>
2018-05-29 10:02:51 +0200

    Network fix

--- truffle.js
+++ truffle.js
@@ -6,7 +6,7 @@ console.log(`ENV ${process.env['HOME']}`)
 module.exports = {
   networks: {
       development: {
-          provider: new Web3.providers.IpcProvider(process.env['HOME'] + "/Library/Ethereum/net42", net),
+          provider: new Web3.providers.IpcProvider(process.env['HOME'] + "/Library/Ethereum/net42/geth.ipc", net),
           network_id: "*",
           gas: 3000000
       }
